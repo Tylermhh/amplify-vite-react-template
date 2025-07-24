c22081d356f3353aa77b8bf19dcd9a9afdf460fd
Tylermhh <tyler.hlaing@gmail.com>
2025-07-24 12:15:27 -0700

    successfully added user privacy logic while testing on cloudSandBox

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,8 @@ const client = generateClient<Schema>();
 
 function App() {
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
-  const { signOut } = useAuthenticator();
+  const { user, signOut } = useAuthenticator();
+  
 
   useEffect(() => {
     client.models.Todo.observeQuery().subscribe({
@@ -26,7 +27,7 @@ function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}'s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
@@ -39,9 +40,9 @@ function App() {
       <div>
         🥳 App successfully hosted. Try creating a new todo.
         <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
+        {/* <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
           Review next step of this tutorial.
-        </a>
+        </a> */}
       </div>
       <button onClick={signOut}>Sign Out</button>
     </main>
